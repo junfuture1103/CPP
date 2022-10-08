6b81d035793a3276d1ae909dfc3e0fe5873170bd
JUNTHEWORLD <62176354+junfuture1103@users.noreply.github.com>
2022-10-08 09:14:53 +0900

    Create cout.cpp

---
+++ Output/cout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cout << "name : ";
+	cout << "juntheworld";
+	cout << "\n";
+	cout << "height : " << 120.5 << "cm" << endl;
+
+	return 0;
+}
